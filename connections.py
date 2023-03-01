2f82e481767228fb7e9dd766a049a5786d87badc
smerchanmole <sfmerchan@gmail.com>
2023-03-01 13:03:00 +0100

    Add existing file

---
+++ connections.py
@@ -0,0 +1,6 @@
+database_ip="YOUR DATABASE IP"
+database_port=5432 #your database port
+database_db="YOUR DATABASE NAME WHERE THE TABLE 'EMBALSE' IS"
+database_user="YOUR DATABASE USER"
+database_password="YOUR DATABASE PASSWORD"
+
